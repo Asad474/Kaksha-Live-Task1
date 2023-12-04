fc247e6bb96428bf1f10a4a4aa06595cc44e18b7
Asad Sarfaraz <asadsarfraz474@gmail.com>
2023-12-04 11:35:10 +0530

    Connected to database successfully.

--- server.js
+++ server.js
@@ -1,25 +1,47 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 const port = 8080;
 
+mongoose.connect(process.env.MONGODB_URL)
+    .then(conn => console.log(`Connected to database connection ${conn.connection.host} successfully`))
+    .catch(error => console.log(error.message));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const items = [];
+const itemSchema = new mongoose.Schema({
+    item: String,
+});
+
+const itemData = mongoose.model('Item', itemSchema);
 
 app.get('/', (req, res) => {
     res.send('Hello');
 });
 
-app.get('/getItems', (req, res) => {
-    res.send(items);
+app.get('/getItems', async(req, res) => {
+    try {
+        const items = await itemData.find({});
+        res.send(items);   
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
 });
 
-app.post('/postItem', (req, res) => {
-    const { item } = req.body;
-    items.push(item);
-    res.send(items);
+app.post('/postItem', async(req, res) => {
+    try{
+        const { item } = req.body;
+        const ob = await itemData.create({ item });
+        res.send(ob);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
 });
 
 app.listen(port, () => {
